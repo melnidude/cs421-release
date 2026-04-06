a549a435c292a157c2bfc9ec01f7ddc29668a33f
nathan <nathan.melnikov@gmail.com>
2026-04-06 11:44:37 -0500

    mp3 done

--- mp3-cps/src/Lib.hs
+++ mp3-cps/src/Lib.hs
@@ -3,7 +3,7 @@
 
 module Lib where
 
-import System.IO (hPutStrLn, hPutStr, stdout, hFlush)
+import System.IO (hPutStrLn, hPutStr, stdout, hFlush, nativeNewline)
 
 import Data.List (intercalate)
 
@@ -64,12 +64,19 @@ ctorShow (AppExp f e)     = "AppExp (" ++ ctorShow f ++ ") (" ++ ctorShow e ++ "
 --- ### `factk :: Integer -> (Integer -> t) -> t`
 
 factk :: Integer -> (Integer -> t) -> t
-factk = undefined
+factk 0 k = k 1
+factk n k = factk (n-1) (\v -> k (n * v))
 
 --- ### `evenoddk :: [Integer] -> (Integer -> t) -> (Integer -> t) -> t`
 
 evenoddk :: [Integer] -> (Integer -> t) -> (Integer -> t) -> t
-evenoddk = undefined
+evenoddk [] ke ko = ke 0
+evenoddk [x] ke ko 
+    | even x = ke x
+    | otherwise = ko x
+evenoddk (x:xs) ke ko 
+    | even x = evenoddk xs (\v -> ke (v + x)) ko
+    | otherwise = evenoddk xs ke (\v -> ko (v + x))
 
 --- Automated Translation
 --- ---------------------
@@ -80,22 +87,72 @@ gensym i = ("v" ++ show i, i + 1)
 --- ### Define `isSimple`
 
 isSimple :: Exp -> Bool
-isSimple = undefined
+isSimple (IntExp _) = True
+isSimple (VarExp _) = True
+isSimple (LamExp s body) = isSimple body
+isSimple (IfExp a b c) = isSimple a && isSimple b && isSimple c
+isSimple (OpExp s a b) = isSimple a && isSimple b
+isSimple (AppExp _ _) = False
 
 --- ### Define `cpsExp` - Overview
 
 cpsExp :: Exp -> Exp -> Integer -> (Exp, Integer)
-cpsExp = undefined
+
 
 --- #### Define `cpsExp` for Integer and Variable Expressions
-
+cpsExp (IntExp i) k n = (AppExp k (IntExp i), n)
+cpsExp (VarExp v) k n = (AppExp k (VarExp v), n)
 --- #### Define `cpsExp` for Application Expressions
-
+cpsExp (AppExp f arg) k n
+    | isSimple arg = (AppExp (AppExp f arg) k, n)
+    | otherwise = 
+        let (v, n1) = gensym n
+            result = LamExp v (AppExp (AppExp f (VarExp v)) k)
+            (arg2, n2) = cpsExp arg result n1 
+        in (arg2, n2)
 --- #### Define `cpsExp` for Operator Expressions
+cpsExp (OpExp op e1 e2) k n
+    | isSimple e1 && isSimple e2 = (AppExp k (OpExp op e1 e2), n) --case 1
 
+    | isSimple e1 = --case 2
+        let (v1, n1) = gensym n
+            result = LamExp v1 (AppExp k (OpExp op e1 (VarExp v1)))
+            (e2_transformed, n2) = cpsExp e2 result n1 
+        in (e2_transformed, n2)
+
+    | isSimple e2 = --case 3
+        let (v2, n1) = gensym n
+            result = LamExp v2 (AppExp k (OpExp op (VarExp v2) e2))
+            (e1_transformed, n2) = cpsExp e1 result n1 
+        in (e1_transformed, n2)
+
+    | otherwise = --case 4
+        let (v1, n1) = gensym n
+            result = LamExp v1 (
+                let (v2, n2) = gensym n1
+                    result2 = LamExp v2 (AppExp k (OpExp op (VarExp v1) (VarExp v2)))
+                    (e2_transformed, n3) = cpsExp e2 result2 n2
+                in e2_transformed)
+            (e1_transformed, n4) = cpsExp e1 result n1
+        in (e1_transformed, n4)
 --- #### Define `cpsExp` for If Expressions
+cpsExp (IfExp a b c) k n
+    | isSimple a = 
+        let (b_t, n1) = cpsExp b k n
+            (c_t, n2) = cpsExp c k n1
+        in (IfExp a b_t c_t, n2)
 
+    | otherwise = 
+        let (v, n1) = gensym n
+            (b_t, n2) = cpsExp b k n1
+            (c_t, n3) = cpsExp c k n2
+            result = LamExp v (IfExp (VarExp v) b_t c_t)
+            (a_t, n4) = cpsExp a result n3
+        in (a_t, n4)
 --- ### Define `cpsDecl`
 
 cpsDecl :: Stmt -> Stmt
-cpsDecl = undefined
+cpsDecl (Decl f args body) =
+    let k = "k"
+        (body_t, _) = cpsExp body (VarExp k) 0
+    in Decl f (args ++ [k]) body_t
